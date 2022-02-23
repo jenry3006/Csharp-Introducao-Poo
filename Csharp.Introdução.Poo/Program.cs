d951988ef0a402918ef16249f55ddcc6cec73b7e
Humberto Petita <humbertopetita@gmail.com>
2022-02-23 19:13:04 -0300

    iniciando com csharp

---
+++ Csharp.Introdução.Poo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Csharp.Introdução.Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Hello Word
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            #endregion
+
+            #region Char e Strings
+
+            char letra = 'B';
+            string frase = "om dia!";
+
+            Console.WriteLine(letra + frase);
+            Console.WriteLine(string.Concat(letra, frase));
+            Console.WriteLine($"{letra}{frase} Jenry");
+            Console.WriteLine(string.Format("{0}{1} Jenry", letra, frase));
+            Console.WriteLine(frase[6]);
+            Console.WriteLine($"Informe seu nome: {Console.ReadLine() }");
+
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
